6595f1b08cecf6ed6aedb34ed75dc5695a21ca4b
CaliforniaHasFallen69 <100180493+CaliforniaHasFallen69@users.noreply.github.com>
2022-03-14 19:46:31 +0700

    Add files via upload

---
+++ Praktikum 3/nomor9.c
@@ -0,0 +1,79 @@
+/* Nama File        : CekBulan.c
+/* Deskripsi        : Menampilkan nama bulan berdasarkan inputan
+/* Nama pembuat     : Zaeri Haikal Rabbani/24060121140159
+/* Tanggal pembuatan: 14 Maret 2022*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+
+    int bulan;
+
+    printf("masukkan bulan : ");
+    scanf("%d",&hari);
+
+    switch(bulan){
+        case 1 :
+            printf("Januari");
+            break;
+        case 2 :
+            printf("Februari");
+            break;
+        case 3 :
+            printf("Maret");
+            break;
+        case 4 :
+            printf("April");
+            break;
+        case 5 :
+            printf("Mei");
+            break;
+        case 6 :
+            printf("Juni");
+            break;
+        case 7 :
+            printf("Juli");
+            break;
+        case 8 :
+            printf("Agustus");
+            break;
+        case 9 :
+            printf("September");
+            break;
+        case 10 :
+            printf("Oktober");
+            break;
+        case 11 :
+            printf("November");
+            break;
+        case 12 :
+            printf("Desember");
+            break;
+        default :
+            printf("salah");
+            break;
+
+    return 0;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
